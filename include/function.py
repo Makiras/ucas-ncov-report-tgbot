c9b9079b9e27466bd31742fe0f428f4dfe756165
Makiras <makiras.x@outlook.com>
2022-08-12 00:25:14 +0800

    [WIP] adapt new api, todo: add settings for custom default 今日是否在校 昨日是否接受核酸检测

--- include/function.py
+++ include/function.py
@@ -41,61 +41,72 @@ def extract_post_data(json_txt: str) -> Dict[str, str]:
     old_data = json.loads(json_txt)['d']
     _logger.debug(f'\nold_data: {old_data}\n')
 
+    # app_id = 4
+    # new_data = {
+    #     'realname': old_data['realname'],
+    #     'number': old_data['number'],
+    #     'szgj_api_info': old_data['szgj_api_info'], 
+    #     'sfzx': old_data['sfzx'], #是否在校
+    #     'szdd': '国内', #所在地点 update 2022.2.9
+    #     'ismoved': 0,  # 如果前一天位置变化这个值会为1，第二天仍然获取到昨天的1，而事实上位置是没变化的，所以置0
+    #     'tw': old_data['tw'], #体温
+    #     'sfcxtz': old_data['sfcxtz'], #todo 是否出现症状
+    #     'sfjcbh': old_data['sfjcbh'],  # 是否接触病患
+    #     'sfcyglq': old_data['sfcyglq'],  # 是否处于隔离期（观察期
+    #     'sfcxzysx': old_data['sfcxzysx'], #
+    #     'geo_api_info': old_data['old_city'],  # 保持昨天的结果
+    #     'old_city': old_data['old_city'],
+    #     'geo_api_infot': old_data['geo_api_infot'],
+    #     'date': datetime.datetime.now(tz=pytz_timezone("Asia/Shanghai")).strftime("%Y-%m-%d"),
+    #     'jcjgqk': old_data['jcjgqk'],  #情况
+    #     'jrsflj': old_data['jrsflj'],  # add @2020.9.16 近日是否离京
+    #     'gtshcyjkzt': old_data['gtshcyjkzt'],  # add @2020.9.16 共同生活人员健康状况
+    #     'jrsfdgzgfxdq': old_data['jrsfdgzgfxdq'],  # add @2020.9.16 近日是否到过中高风险地区
+    #     'app_id': 'ucas'
+    # }
+    # if old_data["jrsflj"]!="否" :
+        # raise RuntimeError(f'近日是否离京不为否。请暂停后手动打卡！')
+
+    # app_id = 9 
     new_data = {
+        'date': datetime.datetime.now(tz=pytz_timezone("Asia/Shanghai")).strftime("%Y-%m-%d"),
         'realname': old_data['realname'],
         'number': old_data['number'],
-        'szgj_api_info': old_data['szgj_api_info'], # 似乎没用？
-        # 'szgj': old_data['szgj'],# 2021.8.1 del 
-        # 'old_sfzx': old_data['sfzx'],# 2021.8.1 del
-        'sfzx': old_data['sfzx'], #是否在校
-        # 'szdd': old_data['szdd'], #所在地点
-        'szdd': '国内', #所在地点 update 2022.2.9
-        # 'sflgj': old_data['sflgj'],
-        'ismoved': 0,  # 如果前一天位置变化这个值会为1，第二天仍然获取到昨天的1，而事实上位置是没变化的，所以置0
-        # 'ismoved': old_data['ismoved'],
-        'tw': old_data['tw'], #体温
-        # 'bztcyy': old_data['bztcyy'], # 2021.8.1 del
-        # 'sftjwh': old_data['sfsfbh'],  # 2020.9.16 del
-        # 'sftjhb': old_data['sftjhb'],  # 2020.9.16 del
-        'sfcxtz': old_data['sfcxtz'], #todo 是否出现症状
-        # 'sfyyjc': old_data['sfyyjc'],# 2021.8.1 del
-        # 'jcjgqr': old_data['jcjgqr'],# 2021.8.1 del
-        # 'sfjcwhry': old_data['sfjcwhry'],  # 2020.9.16 del
-        # 'sfjchbry': old_data['sfjchbry'],  # 2020.9.16 del
-        'sfjcbh': old_data['sfjcbh'],  # 是否接触病患
-        # 'jcbhlx': old_data['jcbhlx'], # 2021.1.29 del 接触病患类型
-        'sfcyglq': old_data['sfcyglq'],  # 是否处于隔离期（观察期
-        # 'gllx': old_data['gllx'],   # 2021.1.29 del 隔离类型
-        'sfcxzysx': old_data['sfcxzysx'], #
-        # 'old_szdd': old_data['szdd'],# 2021.8.1 del
-        'geo_api_info': old_data['old_city'],  # 保持昨天的结果
-        'old_city': old_data['old_city'],
-        'geo_api_infot': old_data['geo_api_infot'],
-        'date': datetime.datetime.now(tz=pytz_timezone("Asia/Shanghai")).strftime("%Y-%m-%d"),
-        # 近14日未离京
-        # 'fjsj': old_data['fjsj'],  # 返京时间# 2021.8.1 del
-        # 'ljrq': old_data['ljrq'],  # 离京日期 add@2021.1.24# 2021.8.1 del
-        # 'qwhd': old_data['qwhd'],  # 去往何地 add@2021.1.24# 2021.8.1 del
-        # 'chdfj': old_data['chdfj'],  # 从何地返京 add@2021.1.24# 2021.8.1 del
-
-        # 'jcbhrq': old_data['jcbhrq'], # del 2021.1.29 接触病患日期
-        # 'glksrq': old_data['glksrq'], # del 2021.1.29 隔离开始日期
-        # 'fxyy': old_data['fxyy'],# 2021.8.1 del
-        # 'jcjg': old_data['jcjg'],# 2021.8.1 del
-        # 'jcjgt': old_data['jcjgt'],# 2021.8.1 del
-        # 'qksm': old_data['qksm'],# 2021.8.1 del
-        # 'remark': old_data['remark'],
-        'jcjgqk': old_data['jcjgqk'],  #情况
-        'jrsflj': old_data['jrsflj'],  # add @2020.9.16 近日是否离京
-        # 'jcwhryfs': old_data['jcwhryfs'],# 2021.8.1 del
-        # 'jchbryfs': old_data['jchbryfs'],# 2021.8.1 del
-        'gtshcyjkzt': old_data['gtshcyjkzt'],  # add @2020.9.16 共同生活人员健康状况
-        'jrsfdgzgfxdq': old_data['jrsfdgzgfxdq'],  # add @2020.9.16 近日是否到过中高风险地区
+        'jzdz': old_data['jzdz'], # 居住地址
+        'zrzsdd': old_data['zrzsdd'], # 昨日居住地点 
+        #todo: add new db column for 今日是否在校 
+        'sfzx': 5, # 是否在校 
+        'dqszdd': old_data['dqszdd'], # 当前所在地点
+        'geo_api_infot': old_data['geo_api_infot'], # 保持昨天的结果
+        'szgj': old_data['szgj'] ,# 所在国家
+        'szgj_select_info[id]': 0 ,# 所在国家
+        'szgj_select_info[name]': '', # 所在国家
+        'geo_api_info': old_data['old_city'], # 保持昨天的结果
+        'dqsfzzgfxdq': old_data['dqsfzzgfxdq'], # 当前是否在中高风险地区
+        'zgfxljs': old_data['zgfxljs'], # 中高风险旅居史
+        'tw': '1', # 体温，不是1就完蛋了
+        'sffrzz': '0' ,# 是否发热症状
+        'dqqk1': 1  ,# 当前情况状态
+        'dqqk1qt':  '',# 当前情况其他
+        'dqqk2': 1 ,# 当前情况健康码
+        'dqqk2qt':  '',# 当前情况其他
+        #todo: add new db column for 昨日是否接受核酸检测
+        'sfjshsjc': 0 ,# （昨日）是否接受核酸检测
+        'dyzymjzqk': old_data['dyzymjzqk'], # 第一针疫苗接种情况
+        'dyzwjzyy':  old_data['dyzwjzyy'] ,# 第一帧未接种原因
+        'dyzjzsj': old_data['dyzjzsj'], # 第一帧疫苗接种时间
+        'dezymjzqk': old_data['dezymjzqk'], # 第二针疫苗接种情况
+        'dezwjzyy': old_data['dezwjzyy'], # 第二帧未接种原因
+        'dezjzsj': old_data['dezjzsj'], # 第二帧疫苗接种时间
+        'dszymjzqk': old_data['dszymjzqk'], # 第三针疫苗接种情况
+        'dszwjzyy': old_data['dszwjzyy'], # 第三帧未接种原因
+        'dszjzsj': old_data['dszjzsj'], # 第三帧疫苗接种时间
+        'gtshryjkzk': old_data['gtshryjkzk'] ,# 共同生活人员健康情况
+        'extinfo': '', # 扩展信息
         'app_id': 'ucas'
     }
 
-    if old_data["jrsflj"]!="否" :
-        raise RuntimeError(f'近日是否离京不为否。请暂停后手动打卡！')
+
 
     return new_data
 
